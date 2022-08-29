84ece852ebb598b1984408f2d10a95460ff051db
felixcesp <j.f.gcespedes@hotmail.com>
2022-08-29 17:33:42 -0500

    despliuegue final

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import Login from './components/Login'
+import { store } from './redux/store/store';
+import { Provider } from 'react-redux';
+import AppRoutes from './routes/AppRoutes';
 
 export default function App() {
   return (
-    <div>
-      <Login/>
-    </div>
+    <Provider store={store}>
+      <AppRoutes/>
+    </Provider>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { store } from './redux/store/store';
-import { Provider } from 'react-redux';
-import AppRoutes from './routes/AppRoutes';
+
+import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <Provider store={store}>
-           <AppRoutes/>
-  </Provider>
+ <App/>
  
   </React.StrictMode>
 
